273d1404a47e6e7f23de3034fad75296affd2a16
Alexey Karcev <krakenlex@mail.ru>
2023-11-10 19:50:14 +0300

    how to parse hyperlink witihin re lib
    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import re
+
+with open('input.html', 'r', encoding='utf-8') as file:
+    html_content = file.read()
+
+pattern = re.compile(r'<a\s+href="(.*?)">')
+matches = re.findall(pattern, html_content)
+
+for match in matches:
+    print(match)
